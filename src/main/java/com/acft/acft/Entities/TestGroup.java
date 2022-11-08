2ab9d4481a5a7e148c0596b8d8e5e11cecafbb87
geoshoouh <113086640+geoshoouh@users.noreply.github.com>
2022-11-08 16:16:31 -0600

    Added import for cascade type
    -clearly, I'm not editing in an IDE

--- src/main/java/com/acft/acft/Entities/TestGroup.java
+++ src/main/java/com/acft/acft/Entities/TestGroup.java
@@ -7,6 +7,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import javax.persistence.CascadeType;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
