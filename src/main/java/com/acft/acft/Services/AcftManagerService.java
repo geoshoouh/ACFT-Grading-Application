d85a2c679e2888d9fd8408111930f2657a4744a7
geoshoouh <113086640+geoshoouh@users.noreply.github.com>
2022-11-08 16:06:48 -0600

    added transactional annotation to flushDB

--- src/main/java/com/acft/acft/Services/AcftManagerService.java
+++ src/main/java/com/acft/acft/Services/AcftManagerService.java
@@ -179,6 +179,7 @@ public class AcftManagerService {
         return file;
     }
         
+    @Transactional
     public boolean flushDatabase(){
         soldierRepository.deleteAll();
         testGroupRepository.deleteAll();
