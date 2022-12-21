02218b1924f910717ff2b85a04fd66fbd78cb73c
geoshoouh <joshua.d.tate12@gmail.com>
2022-12-20 18:08:37 -0600

    troubleshoot db

--- src/main/java/com/acft/acft/Entities/Soldier.java
+++ src/main/java/com/acft/acft/Entities/Soldier.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.SequenceGenerator;
 
@@ -24,6 +25,7 @@ public class Soldier {
 
     @JsonIgnore
     @ManyToOne
+    @JoinColumn(name = "TESTGROUP")
     private TestGroup testGroup;
 
 
