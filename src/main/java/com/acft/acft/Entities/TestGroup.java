533ea3c4f3caaab37bf298db726742488194fd13
geoshoouh <joshua.d.tate12@gmail.com>
2022-12-21 14:54:47 -0600

    removed independent ID sequences

--- src/main/java/com/acft/acft/Entities/TestGroup.java
+++ src/main/java/com/acft/acft/Entities/TestGroup.java
@@ -23,7 +23,7 @@ import java.time.temporal.ChronoUnit;
 public class TestGroup {
     
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
 
     private Long pseudoId;
